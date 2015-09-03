5733be64eecb67f06ea3e5c0abfd86b1abc5928e
Avijit Mandal <avijit@codeapogee.com>
2015-09-03 12:38:26 +0530

    Project library

---
+++ app/controllers/book_controller.rb
@@ -0,0 +1,55 @@
+class BookController < ApplicationController
+   def list
+      @books = Book.all
+   end
+
+   def show
+      @book = Book.find(params[:id])
+
+   end
+
+   def new
+      @book = Book.new
+      @subjects = Subject.all
+   end
+
+   def create
+      
+      @book = Book.new(book_params)
+      if @book.save
+            redirect_to :action => 'list'
+      else
+            @subjects = Subject.all
+            render :action => 'new'
+      end
+   end
+
+   def edit
+      @book = Book.find(params[:id])
+      @subjects = Subject.all
+   end
+
+   def update
+      @book = Book.find(params[:id])
+      if @book.update_attributes(book_params)
+         redirect_to :action => 'show', :id => @book
+      else
+         @subjects = Subject.all
+         render :action => 'edit'
+      end
+   end
+
+   def destroy
+      Book.find(params[:id]).destroy
+      redirect_to :action => 'list'
+   end
+
+   def show_subject
+      @subject = Subject.find(params[:subject_id])
+   end
+
+   private
+      def book_params
+         params.require(:book).permit(:title, :price, :subject_id, :description)
+      end
+end
---
+++ db/migrate/20150829090516_create_books.rb
@@ -0,0 +1,15 @@
+class CreateBooks < ActiveRecord::Migration
+  def change
+    create_table :books do |t|
+    	t.column :title, :string, :limit => 32, :null => false 
+		t.column :price, :float 
+		t.column :subject_id, :integer 
+		t.column :description, :text
+
+      t.timestamps null: false
+    end
+  end
+  def self.down
+    drop_table :books
+  end
+end
