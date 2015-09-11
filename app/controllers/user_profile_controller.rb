236912ecb45c2043098412237ae02928293bdbf7
Avijit Mandal <avijit@codeapogee.com>
2015-09-11 19:12:57 +0530

    Profile picture upload

--- app/controllers/user_profile_controller.rb
+++ app/controllers/user_profile_controller.rb
@@ -7,7 +7,9 @@ class UserProfileController < ApplicationController
 	end
 
 	def create
+		
 		@user_profile = UserProfile.new(user_params)
+		@user_profile.profile_image = params[:profile_image]
 		@user_profile.user_id = current_user.id
 		if @user_profile.save
 			redirect_to user_profile_path(@user_profile)
@@ -17,6 +19,8 @@ class UserProfileController < ApplicationController
 	end
 
 	def show
+		@uploader = AvatarUploader.new
+
 		@user_profile = UserProfile.find(params[:id])
 	end
 
@@ -25,7 +29,9 @@ class UserProfileController < ApplicationController
 	end
 
 	def update
+		
     	@user_profile = UserProfile.find(params[:id])
+    	@user_profile.profile_image = params[:profile_image]
 
     	if @user_profile.update_attributes(user_params)
         	redirect_to user_profile_path(@user_profile)
@@ -37,7 +43,7 @@ class UserProfileController < ApplicationController
 	private
 
 		def user_params
-			params.require(:user_profile).permit(:user_id, :phone_no, :sex, :date_of_birth, :address)
+			params.require(:user_profile).permit(:user_id, :phone_no, :sex, :date_of_birth, :address , :profile_image)
 		end
 
 
--- app/models/user_profile.rb
+++ app/models/user_profile.rb
@@ -1,6 +1,8 @@
 class UserProfile < ActiveRecord::Base
 	belongs_to :user
 
+	mount_uploader :profile_image, AvatarUploader
+
 	validates_presence_of :phone_no #:message=>"Must enter your phone no"
 	validates :sex, :presence =>  true
 	validates :date_of_birth, :presence => true
